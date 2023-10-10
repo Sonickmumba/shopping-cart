d7908d6904ff622b0828482059f315c53246ae4a
Sonick Mumba <kingellie.mumba@gmail.com>
2023-10-10 20:29:35 +0200

    Add color to the cart button

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
     <>
       <div className="card">
         <Header />
-        <p className="try">thanks sonick </p>
       </div>
     </>
   )
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <button type="button">About</button>
       </div>
       <div className="shop-div flex">
-      <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Shop</button>
+        <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Shop</button>
         <button className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-4 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Your Carts <span className="inline-flex items-center justify-center w-5 h-4 p-2 ml-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">20</span></button>
       </div>
       
