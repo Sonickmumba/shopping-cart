38d8c089b4694aaa73785a16d858cdd7b90b7b6d
Sonick Mumba <kingellie.mumba@gmail.com>
2023-10-10 19:44:34 +0200

    Add home icon

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+
+const Header = () => {
+  return (
+    <nav className="nav-container flex justify-between mx-auto bg-gray-200 p-4">
+      <div className="div-header-icon">
+        <img src="" alt="" />
+        <span className="material-symbols-outlined">home</span>
+        <button type="button">About</button>
+      </div>
+      <div>
+        shop and cart with counter button
+      </div>
+      
+    </nav>
+  )
+}
+
+export default Header
