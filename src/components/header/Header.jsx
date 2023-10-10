3a2a6d2c970b7c95bc2c7bd2abecb4b36a6daccf
Sonick Mumba <kingellie.mumba@gmail.com>
2023-10-10 20:26:07 +0200

    Implement the Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,14 @@
 
 const Header = () => {
   return (
-    <nav className="nav-container flex justify-between mx-auto bg-gray-200 p-4">
-      <div className="div-header-icon">
-        <img src="" alt="" />
-        <span className="material-symbols-outlined">home</span>
+    <nav className="nav-container flex justify-between mx-aut p-4">
+      <div className="div-header-icon flex items-center w-30">
+        <span className="material-symbols-outlined mr-3">home</span>
         <button type="button">About</button>
       </div>
-      <div>
-        shop and cart with counter button
+      <div className="shop-div flex">
+      <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Shop</button>
+        <button className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-4 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Your Carts <span className="inline-flex items-center justify-center w-5 h-4 p-2 ml-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">20</span></button>
       </div>
       
     </nav>
