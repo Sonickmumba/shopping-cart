8386ce4ec1bf63cfce96b0842e10b21fbb4d5b64
Sonick Mumba <kingellie.mumba@gmail.com>
2023-10-11 08:29:40 +0200

    Implement shopping component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <nav className="nav-container flex justify-between mx-aut p-4">
+    <nav className="nav-container flex justify-between mx-auto p-4">
       <div className="div-header-icon flex items-center w-30">
         <Link to="/"><span className="material-symbols-outlined mr-3">home</span></Link>
         <Link to="/about"><button type="button">About</button></Link>
@@ -12,6 +12,7 @@ const Header = () => {
         <Link to="/carts"><button className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-4 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Your Carts <span className="inline-flex items-center justify-center w-5 h-4 p-2 ml-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">20</span></button></Link>
       </div>
     </nav>
+
   )
 }
 
