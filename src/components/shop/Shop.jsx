7f7c08762b51a45d128928ba0a3c09b6b38f4fb4
Mumba <27748812@sun.ac.za>
2023-10-14 00:25:13 +0200

    Remove console.log

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -34,10 +34,6 @@ const Shop = () => {
   };
   
   useEffect(() => {
-    console.log("Updated Cart Data:", cartData);
-  }, [cartData]);
-
-  useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch("https://fakestoreapi.com/products?limit=5");
