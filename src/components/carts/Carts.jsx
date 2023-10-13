cf10c976e33d638f227c3e8a32e90665820d728b
Mumba <27748812@sun.ac.za>
2023-10-14 00:33:38 +0200

    Update the carts compoent

--- src/components/carts/Carts.jsx
+++ src/components/carts/Carts.jsx
@@ -1,22 +1,50 @@
 import PropTypes from "prop-types";
 
-const Carts = ({ cartData }) => {
-  // const { carts } = props;
+// const Carts = ({ cartData }) => {
+//   // const { carts } = props;
 
-  // In Carts component
+//   // In Carts component
   
+//   return (
+//     <div>
+//       {cartData && cartData.length > 0 ? (
+//         cartData.map((item) => (
+//           <div key={item.id}>{item.title}</div>
+//         ))
+//       ) : (
+//         <p>Your cart is empty</p>
+//       )}
+//     </div>
+//   )
+// }
+
+// Carts.jsx
+import React from "react";
+
+const Carts = ({ cartData }) => {
+  const totalItems = cartData ? cartData.reduce((acc, item) => acc + item.cartNumber, 0) : 0;
+
   return (
     <div>
+      <p>Your Cart ({totalItems} items)</p>
       {cartData && cartData.length > 0 ? (
-        cartData.map((item) => (
-          <div key={item.id}>{item.title}</div>
-        ))
+        <div>
+          {cartData.map((item) => (
+            <div key={item.id}>
+              <p>{item.title} - Quantity: {item.cartNumber}</p>
+            </div>
+          ))}
+          <button onClick={() => handleProceedToPayment()}>Proceed to Payment</button>
+        </div>
       ) : (
         <p>Your cart is empty</p>
       )}
     </div>
-  )
-}
+  );
+};
+
+// export default Carts;
+
 
 Carts.propTypes = {
   cartData: PropTypes.arrayOf(
