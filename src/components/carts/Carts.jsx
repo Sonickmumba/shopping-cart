0612fa87b1fcf6af521d84fd8d9db60a11e74212
Sonick Mumba <kingellie.mumba@gmail.com>
2023-10-15 12:44:59 +0200

    Update carts component

--- src/components/carts/Carts.jsx
+++ src/components/carts/Carts.jsx
@@ -2,11 +2,10 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 const Carts = ({ cartData }) => {
-  // const totalItems = cartData ? cartData.reduce((acc, item) => acc + item.cartNumber, 0) : 0;
   const totalCost = cartData
     ? cartData.reduce((cost, item) => cost + item.price, 0)
     : 0;
-  const handleChange = (e, id) => {
+  const handleChange = (e,id) => {
     e.preventDefault();
     console.log(id);
   }
@@ -45,7 +44,7 @@ const Carts = ({ cartData }) => {
                           <span className="font-semibold">-</span>
                           <input
                             type="text"
-                            onChange={(e) => handleChange(e, item.id)}
+                            onChange={(e) => handleChange(e,item.id)}
                             className="focus:outline-none bg-gray-100 border h-6 w-8 rounded text-sm px-2 mx-2"
                             value="1"
                           />
